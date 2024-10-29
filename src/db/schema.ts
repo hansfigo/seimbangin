f27836858440d9be96a06b92fdaa02afac9a2b4b
HansFigo <hambasahaya0303@gmail.com>
2024-10-29 15:34:21 +0700

    update user table schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,18 +1,35 @@
-import { mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
+import {
+  int,
+  mysqlEnum,
+  mysqlTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/mysql-core";
 
 // user table schema
 export const usersTable = mysqlTable("users", {
   id: serial().primaryKey(),
+  role: int().notNull().default(0).notNull(), // 0 for user, 1 for admin
   first_name: varchar({ length: 255 }).notNull(),
   last_name: varchar({ length: 255 }).notNull(),
   username: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  password : varchar({ length: 255 }).notNull(),
+  password: varchar({ length: 255 }).notNull(),
+  profilePicture: varchar("profile_picture", { length: 256 }),
+  createdAt: timestamp("created_at"),
+  updatedAt: timestamp("updated_at"),
 });
 
 // export const transactionsTable = mysqlTable("transactions", {
 //   id: serial().primaryKey(),
-//   user_id: int().notNull(),
+//   user_id: int()
+//     .references(() => usersTable.id)
+//     .notNull(),
+//   type: int().notNull().default(0), // 0 for income , 1 for expense
 //   amount: int().notNull(),
-//   created_at: varchar({ length: 255 }).notNull(),
+//   description: text("description"),
+//   createdAt: timestamp("created_at"),
+//   updatedAt: timestamp("updated_at"),
 // });
