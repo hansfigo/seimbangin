ab828170b0b7c2722212206c279c1908196e1677
HansFigo <hambasahaya0303@gmail.com>
2024-10-29 16:44:55 +0700

    update drizzle script

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,16 +7,21 @@ const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const database = process.env.DB_NAME;
+import * as schema from './schema';
 
 // create a connection pool
-const poolConnection = mysql.createPool({
+export const poolConnection = mysql.createPool({
   host: host || "",
   user: user || "",
   password: password || "",
   database: database || "",
+  multipleStatements: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // create a drizzle instance
-const db = drizzle({ client: poolConnection });
+const db = drizzle(poolConnection, { schema, mode: 'default' });
 
 export default db;
